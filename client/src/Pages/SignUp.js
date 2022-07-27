8ffce881c31adc4beb6d153221ff6ba6532d4847
kareem5378 <kareem.shaweesh077@gmail.com>
2022-07-27 16:46:46 +0300

    newwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

--- client/src/Pages/SignUp.js
+++ client/src/Pages/SignUp.js
@@ -3,19 +3,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const SignUp = () => {
-  const [First_Name, setFirstName] = useState("");
-  const [Last_Name, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
   const [data, setUsers] = useState("");
   const [Role, setRole] = useState("");
-  const [Company_ID, setCompanyID] = useState("");
+  const [companyID, setCompanyID] = useState("");
 
   const handleSignUp = (e) => {
     console.log("Hello");
 
-    const data = { First_Name, Last_Name, Email, Password, Role,Company_ID };
-    sessionStorage.setItem('user',JSON.stringify( data))
+    const data = { firstName, lastName, Email, Password, Role, companyID };
+
+    console.log(data);
+    localStorage.setItem("firstName", firstName);
+    localStorage.getItem(firstName);
+    localStorage.setItem("last_Name", lastName);
+    localStorage.getItem(lastName);
+    localStorage.setItem("Email", Email);
+    localStorage.getItem(Email);
+    localStorage.setItem("Password", Password);
+    localStorage.getItem(Password);
+    localStorage.setItem("Role", Role);
+    localStorage.getItem(Role);
+    alert("Welcome " + localStorage.getItem("firstName"));
     const fetchUsers = () => {
       axios.post("http://localhost:5000/api/user/signup", data).then((res) => {
         console.log("Hello");
@@ -26,19 +38,19 @@ const SignUp = () => {
   return (
     <main style={{ padding: "1rem 0" }}>
       <div className="signupboxescontainer">
-        <h1>Sign up</h1>
+        <h1>Sign p</h1>
         <br />
         <div className="signupboxes">
           <input
             type={"text"}
-            value={First_Name}
+            value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required
             placeholder="First name"
           ></input>
           <input
             type={"text"}
-            value={Last_Name}
+            value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             required
             placeholder="Last name"
